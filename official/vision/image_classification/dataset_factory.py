c98e03dc4fd2ddb2acd0ef6dcd903a55cc1e2e76
Donglin Zhuang <donglinzhuang@outlook.com>
2021-02-26 06:53:41 +0000

    tf2.1 distrubution compatibility

--- official/vision/image_classification/dataset_factory.py
+++ official/vision/image_classification/dataset_factory.py
@@ -288,8 +288,11 @@ class DatasetBuilder:
                      '%d devices.',
                      strategy.num_replicas_in_sync,
                      self.config.num_devices)
-      dataset = strategy.experimental_distribute_datasets(
-          self._build())
+      # dataset = strategy.experimental_distribute_dataset(
+      #     self._build())
+      # dataset = strategy.experimental_distribute_datasets_from_function(
+      #     self._build)
+      dataset = self._build()
     else:
       dataset = self._build()
 
