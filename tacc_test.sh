80644d188944e1d78a934353fb787895bb42e721
Donglin Zhuang <donglinzhuang@outlook.com>
2021-02-28 12:23:01 +0000

    tacc test

--- tacc_test.sh
+++ tacc_test.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-#SBATCH -J testjob           # Job name
-#SBATCH -o testjob.o%j       # Name of stdout output file
-#SBATCH -e testjob.e%j       # Name of stderr error file
+#SBATCH -J imagenet_test           # Job name
+#SBATCH -o imagenet_test.log       # Name of stdout output file
 #SBATCH -p development          # Queue (partition) name
 #SBATCH -N 1               # Total # of nodes (must be 1 for serial)
-#SBATCH -n 10              # Total # of mpi tasks (should be 1 for serial)
+#SBATCH -n 1              # Total # of mpi tasks (should be 1 for serial)
 #SBATCH -t 00:30:00        # Run time (hh:mm:ss)
 #SBATCH --mail-user=donglinzhuang@outlook.com
 #SBATCH --mail-type=all    # Send email at begin and end of job
 
 # Other commands must follow all #SBATCH directives...
 
-module list
-pwd
-date
-sleep 10
+conda activate py37
+export PHTHONPATH="./"
+
+python ./official/vision/image_classification/classifier_trainer.py --mode=train_and_eval --model_type=resnet --dataset=imagenet --model_dir=./ckpt_h1/ --data_dir=/scratch/07464/donglinz/datasets/ILSVRC2012 --deterministic_init --deterministic_input --config_file=./official/vision/image_classification/configs/examples/resnet/imagenet/gpu.yaml --project test --group ff_randtf
